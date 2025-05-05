a139560e774808989067a8537e633ca3abd561f1
Oumar Cloud <oumar.cisse@supdevinci-edu.fr>
2025-05-05 16:07:00 +0200

    test4

--- pneapp/urls.py
+++ pneapp/urls.py
@@ -6,5 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.acceuil, name='acceuil'),
         path('produits/', views.produits_view, name='produits'),
+                path('about/', views.about, name='about'),
+                                path('contact/', views.contact, name='contact'),
+
+
 
 ]
--- pneapp/views.py
+++ pneapp/views.py
@@ -4,6 +4,12 @@ from .models import Produit
 def acceuil(request):
     return render(request, 'acceuil.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
 def produits_view(request):
     produits = Produit.objects.all()
     return render(request, 'produits.html', {'produits': produits})
